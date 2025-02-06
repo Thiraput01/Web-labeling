b26e0d962b4feab8ca54f0395ac1194698e5781b
Thiraput01 <Thiraputball@gmail.com>
2025-02-06 14:51:38 +0700

    feature:update frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,22 @@
+"use client";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
-  return <>hello</>;
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/login");
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen flex-col gap-3">
+      <h1 className="text-2xl">Welcome to web labeling</h1>
+      <button
+        onClick={handleClick}
+        className=" bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded"
+      >
+        Go to Login
+      </button>
+    </div>
+  );
 }
